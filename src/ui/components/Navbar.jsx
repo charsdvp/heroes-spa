7d361511a43da7c8e9601ab40ef4e189344aa9cf
charsdvp <carlossanchezdvp@gmail.com>
2023-11-07 20:14:37 -0600

    añadiendo  useNavigate

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,6 +1,13 @@
-import { Link, NavLink } from 'react-router-dom'
+import { Link, NavLink, useNavigate } from 'react-router-dom'
 
 export const Navbar = () => {
+  // customHook de react-router
+  const navigate = useNavigate()
+  const onLogout = () => {
+    navigate('/login', {
+      replace: true
+    })
+  }
   return (
     <nav className='p-2 navbar navbar-expand-sm navbar-dark bg-dark'>
       <Link
@@ -25,7 +32,9 @@ export const Navbar = () => {
       <div className='navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end'>
         <ul className='navbar-nav ml-auto'>
           <span className='nav-item nav-link text-info'>Carlos</span>
-          <button className='nav-item nav-link btn'>Logout</button>
+          <button
+            className='nav-item nav-link btn'
+            onClick={onLogout}>Logout</button>
         </ul>
       </div>
     </nav>
