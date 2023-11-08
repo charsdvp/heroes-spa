128a715feef6eaa097648afde1758ca6e4a86309
charsdvp <carlossanchezdvp@gmail.com>
2023-11-08 13:10:10 -0600

    update searchComponent

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,16 +1,39 @@
+import { useLocation, useNavigate } from 'react-router-dom'
+import { useForm } from '../hooks/useForm'
+import queryString from 'query-string'
+
 export const SearchPage = () => {
+  const navigate = useNavigate()
+  // para manejar los query params
+  const location = useLocation() // nos dara nuestra localizacion y nuestros params
+  // procesamos nuestros querys
+  const { q = '' } = queryString.parse(location.search)
+  // reutilizamos nuestro hook useform para controlar el form
+  const { searchtext, onInputChange } = useForm({
+    searchtext: ''
+  })
+  // cuando enviamos nuestros valores del form
+  const onSearchSubmit = (event) => {
+    event.preventDefault()
+    // limpiamos el texto
+    if (searchtext.trim().length <= 1) return
+    // navegamos hacia -> searchtext
+    navigate(`?q=${searchtext.toLowerCase().trim()}`)
+  }
   return (
     <>
       <h1>SearchPage</h1>
       <div className="row">
         <div className='col-5'>
           <h4>Searching...</h4>
-          <form>
+          <form onSubmit={ onSearchSubmit }>
             <input type='text'
               placeholder='Search a hero'
               className='form-control'
               name='searchtext'
-              autoComplete='off' />
+              autoComplete='off'
+              value={searchtext}
+              onChange={onInputChange} />
             <button className='btn btn-outline-primary mt-1'>Search</button>
           </form>
         </div>
@@ -20,7 +43,7 @@ export const SearchPage = () => {
             Search a hero
           </div>
           <div className='alert alert-danger'>
-            Theres no results
+            No hero with <b>{q}</b>
           </div>
         </div>
       </div>
