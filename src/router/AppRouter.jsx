75b3d91ff773191821427565d80fefb6bd33ffea
charsdvp <carlossanchezdvp@gmail.com>
2023-11-13 14:31:32 -0600

    rutas privadas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,17 +1,23 @@
 import { Route, Routes } from 'react-router-dom'
 import { LoginPage } from '../auth'
 import { HeroesRoutes } from '../heroes'
+import { PrivateRoute } from './PrivateRoute'
 
 // router principal
 export const AppRouter = () => {
   return (
     <>
-        {/* Definimos nuestras rutas de autenticacion */}
-        <Routes>
-          <Route path='/login' element={<LoginPage />} />
-          {/* Creamos una ruta que apuntara a nuestro heroesRoutes */}
-          <Route path='/*' element={<HeroesRoutes />}/>
-        </Routes>
+      {/* Definimos nuestras rutas de autenticacion */}
+      <Routes>
+        <Route path='login' element={<LoginPage />} />
+        {/* Creamos una ruta que apuntara a nuestro heroesRoutes */}
+        {/* Añadimos nuestras rutdas privadas */ }
+        <Route path='/*' element={
+          <PrivateRoute>
+            <HeroesRoutes />
+          </PrivateRoute>
+          }/>
+    </Routes >
     </>
   )
 }
